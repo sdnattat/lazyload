c4cef086adda4d770e6cd632739f71843886e000
sdnattat <56126962+sdnattat@users.noreply.github.com>
2021-09-24 04:03:47 +0300

    Update policy.js

--- policy.js
+++ policy.js
@@ -1,17 +1,150 @@
 document.querySelector('button.Get').addEventListener('click',()=&gt;{
  
 let all =`<center>
-<p style="text-align: justify;">خصوصيتك أيها الزائر لها أهمية بالغة بالنسبة لنا ، و سياسة الخصوصية الموجودة في هذه الوثيقة تمثل الخطوط العريضة لأنواع المعلومات الشخصية التي يجمعها موقع <a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a> وكيفية استخدام هذه المعلومات.</p><p style="text-align: justify;">نحن نستعين بشركات إعلان كطرف ثالث لعرض الإعلانات ، وعندما تزور موقع <a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a> فـ إنه يحق لهذه الشركات أن تستخدم معلومات حول زياراتك لهذا الموقع ( باستثناء الاسم أو العنوان أو عنوان البريد الإلكتروني أو رقم الهاتف ) وذلك من أجل تقديم إعلانات حول البضائع والخدمات التي تهمك عن طريق ملف تعريف الإرتباط.</p><p style="text-align: justify;">نحن في <a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a> نستخدم <a href="https://ads.google.com/intl/ar_dz/home/">إعلانات Google</a> بصفتها مورِّدًا مالياً خارجياً ، ولذلك تستخدم شركة جوجل ملفات تعريف الارتباط لعرض الإعلانات على موقعنا.
-وبذلك ستتمكّن Google ، باستخدام <a href="https://support.google.com/accounts/answer/61416?hl=ar&amp;co=GENIE.Platform%3DAndroid">ملف تعريف الارتباط DART</a> ، من عرض الإعلانات "حسب الاهتمامات" للمستخدمين استنادًا إلى زياراتهم لموقعنا.</p><p style="text-align: justify;">ويمكن لزوارنا الكرام تعطيل استخدام <a href="https://support.google.com/accounts/answer/61416?hl=ar&amp;co=GENIE.Platform%3DAndroid">ملف تعريف الارتباط</a> DART بزيارة سياسة الخصوصية الخاصة بإعلانات Google وشبكة المحتوى.
-ملفات الدخول:شأنها في ذلك شأن معظم خوادم المواقع الأخرى ، ومن هنا فإن موقع <a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a> يستخدم نظام ملفات الدخول ، وهذا يشمل بروتوكول الانترنت (عناوين ، نوع المتصفح ، مزود خدمة الانترنت "مقدمي خدمات الانترنت" ،  التاريخ / الوقت ، وعدد النقرات لتحليل الاتجاهات).</p><p style="text-align: justify;">من خلال هذه العملية لا يقصد بذلك جمع كل هذه المعلومات في سبيل التلصص على أمور الزوار الشخصية ، وإنما هي أمور تحليلية لأغراض تحسين جودة الإعلانات من قبل Google ، ويضاف إلى ذلك أن جميع هذه المعلومات المحفوظة من قبلنا سرية تماما، وتبقى ضمن نطاق التطوير والتحسين الخاص بموقعنا فقط.
-الكوكيز وإعدادات الشبكة:
-إن شركة Google تستخدم تقنية الكوكيز لتخزين المعلومات عن إهتمامات الزوار، إلى جانب سجل خاص للمستخدم تسجل فيه معلومات محددة عن الصفحات التي تم الوصول إليها أو زيارتها، وبهذه الخطوة فإننا نعرف مدى اهتمامات الزوار وأي المواضيع الأكثر تفضيلا من قبلهم حتى نستطيع بدورنا تطوير محتوانا الخدمي والمعرفي المناسب لهم.
-نضيف إلى ذلك أن بعض الشركات التي تعلن في <a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a> قد تتطلع على الكوكيز وإعدادات الشبكة الخاصة بموقعنا وبكم ، ومن هذه الشركات مثلاً شركة Google وبرنامجها الإعلاني <a href="https://www.google.com/adsense/">Google AdSense</a> وهي شركة الإعلانات الأولى في موقعنا.</p><p style="text-align: justify;">وبالطبع فمثل هذه الشركات المعلنة والتي تعتبر الطرف الثالث في سياسة الخصوصية فهي تتابع مثل هذه البيانات والإحصائيات عبر بروتوكولات الانترنت لأغراض تحسين جودة إعلاناتها وقياس مدى فعاليتها.</p><p style="text-align: justify;">كما أن هذه الشركات بموجب الاتفاقيات المبرمة معنا يحق لها استخدام وسائل تقنية مثل ( الكوكيز ، و إعدادات الشبكة ، و أكواد برمجية خاصة "جافا سكربت" ) لنفس الأغراض المذكورة أعلاه والتي تتلخص في تطوير المحتوى الإعلاني لهذه الشركات وقياس مدى فاعلية هذه الإعلانات ، من دون أي أهداف أخرى قد تضر بشكل أو بآخر على زوار موقعنا.
-وبالطبع فإن موقع <a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a> لا يستطيع الوصول أو السيطرة على هذه الملفات، وحتى بعد سماحك وتفعيلك لأخذها من جهازك (الكوكيز) ، كما أننا غير مسؤولين بأي شكل من الأشكال عن الاستخدام غير الشرعي لها إن حصل ذلك .
-عليك مراجعة سياسة الخصوصية الخاصة بالطرف الثالث في هذه الوثيقة  ( الشركات المعلنة مثل <a href="https://www.google.com/adsense/">Google AdSense</a> ) أو خوادم الشبكات الإعلانية لمزيد من المعلومات عن ممارساتها وأنشطتها المختلفة .</p><p style="text-align: justify;">لمراجعة سياسة الخصوصية للبرنامج الإعلاني <a href="https://www.google.com/adsense/">Google AdSense</a> والتابع لشركة Google يرجى النقر هنا
-يُمكن جمع البيانات لتجديد النشاط التسويقي على الشبكة الإعلانية وشبكة البحث عبر  سياسة الإعلانات التي تستهدف الاهتمامات والمواقع الخاصة بالزوار ، ويمكن تعطيل هذه الخاصية من هنا.
-وأخيرا .. نحن ملزمون ضمن بنود هذه الاتفاقية بان نبين لك كيفية تعطيل خاصية الكوكيز ، حيث يمكنك فعل ذلك من خلال خيارات المتصفح الخاص بك، أو من خلال متابعة  سياسة الخصوصية الخاصة بإعلانات Google وشبكة المحتوى .</p><p style="text-align: justify;">إذا كنت بحاجة إلى مزيد من المعلومات أو لديك أية أسئله عن سياسة الخصوصية ، لا تتردد في الاتصال بنا عن طريق نموذج الإتصال ، بالدخول إلى تبويب اتصل بنا.
-بنود هذه السياسة قابلة للتطوير والتغيير في محتواها في أي وقت نراه ضرورياً .</p>
+<div dir="rtl" style="text-align: right;" trbidi="on">
+<div dir="rtl" style="text-align: right;" trbidi="on">
+<style type="text/css">
+
+
+h2,h3,p{text-align:right;}
+a{text-decoration:none; font-size:16px; color:#00F; text-align:right;}
+
+
+</style>
+
+<br />
+<div style="line-height: 2; text-align: justify;">
+<div class="separator" style="clear: both; text-align: center;">
+<a href="https://1.bp.blogspot.com/-tgHnRIcNQJo/Xpm4mkgK8PI/AAAAAAAAEss/h5S2YNOjIZQh38TggdBnX-08K6QmFXgNACLcBGAsYHQ/s1600/%25D8%25B3%25D9%258A%25D8%25A7%25D8%25B3%25D8%25A9-%25D8%25A7%25D9%2584%25D8%25AE%25D8%25B5%25D9%2588%25D8%25B5%25D9%258A%25D8%25A9.png" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="132" data-original-width="320" src="https://1.bp.blogspot.com/-tgHnRIcNQJo/Xpm4mkgK8PI/AAAAAAAAEss/h5S2YNOjIZQh38TggdBnX-08K6QmFXgNACLcBGAsYHQ/s1600/%25D8%25B3%25D9%258A%25D8%25A7%25D8%25B3%25D8%25A9-%25D8%25A7%25D9%2584%25D8%25AE%25D8%25B5%25D9%2588%25D8%25B5%25D9%258A%25D8%25A9.png" /></a></div>
+<br />
+خصوصيتك أيها الزائر لها أهمية بالغة بالنسبة لنا ، و سياسة الخصوصية الموجودة في هذه الوثيقة تمثل الخطوط العريضة لأنواع المعلومات الشخصية التي يجمعها موقع 1<a href="https://www.joker0o.de/" target="_blank"><u><b><a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a>
+</b></u></a>1 وكيفية استخدام هذه المعلومات.<br />
+<div style="line-height: 2; text-align: justify;">
+نحن نستعين بشركات إعلان كطرف ثالث لعرض الإعلانات ، وعندما تزور موقع <a href="https://www.joker0o.de/" target="_blank"><u><b><a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a>
+</b></u></a> فـ إنه يحق لهذه الشركات أن تستخدم معلومات حول زياراتك لهذا الموقع ( باستثناء الاسم أو العنوان أو عنوان البريد الإلكتروني أو رقم الهاتف ) وذلك من أجل تقديم إعلانات حول البضائع والخدمات التي تهمك عن طريق&nbsp;<b><a href="https://support.google.com/adsense/answer/7549925">ملف تعريف الإرتباط</a></b><b>.<br /></b>
+<br /></div>
+<div style="line-height: 2; text-align: justify;">
+نحن في&nbsp;<a href="https://www.joker0o.de/" target="_blank"><u><b><a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a>
+</b></u></a>&nbsp;نستخدم إعلانات Google بصفتها مورِّدًا مالياً خارجياً ، ولذلك تستخدم شركة جوجل&nbsp;<b><a href="https://support.google.com/adsense/answer/7549925">ملف تعريف الإرتباط</a></b>&nbsp;لعرض الإعلانات على موقعنا.</div>
+<div style="line-height: 2; text-align: justify;">
+<br /></div>
+<div style="line-height: 2; text-align: justify;">
+وبذلك ستتمكّن Google ، باستخدام ملف تعريف الارتباط <b>DART</b> ، من عرض الإعلانات "حسب الاهتمامات" للمستخدمين استنادًا إلى زياراتهم لموقعنا.</div>
+<div style="text-align: justify;">
+<br /></div>
+<div style="line-height: 2; text-align: justify;">
+ويمكن لزوارنا الكرام تعطيل استخدام ملف تعريف الارتباط <b>DART</b> بزيارة <a href="http://www.google.com/policies/technologies/ads/" target="_blank">سياسة الخصوصية الخاصة بإعلانات Google وشبكة المحتوى</a>.</div>
+<div>
+<div style="text-align: justify;">
+<h4>
+<b><span style="font-size: large;">ملفات الدخول:</span></b></h4>
+</div>
+<div style="line-height: 2; text-align: justify;">
+شأنها في ذلك شأن معظم خوادم المواقع الأخرى ، ومن هنا فإن موقع&nbsp;<a href="https://www.joker0o.de/" target="_blank"><u><b><a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a>
+</b></u></a>&nbsp;يستخدم نظام ملفات الدخول ، وهذا يشمل بروتوكول الانترنت (عناوين ، نوع المتصفح ، مزود خدمة الانترنت "مقدمي خدمات الانترنت" ، &nbsp;التاريخ / الوقت ، وعدد النقرات لتحليل الاتجاهات).</div>
+</div>
+<div>
+<div style="text-align: justify;">
+<br /></div>
+</div>
+<div>
+<div>
+<div style="line-height: 2; text-align: justify;">
+من خلال هذه العملية لا يقصد بذلك جمع كل هذه المعلومات في سبيل التلصص على أمور الزوار الشخصية ، وإنما هي أمور تحليلية لأغراض تحسين جودة الإعلانات من قبل <b>Google</b> ، ويضاف إلى ذلك أن جميع هذه المعلومات المحفوظة من قبلنا سرية تماما، وتبقى ضمن نطاق التطوير والتحسين الخاص بموقعنا فقط.</div>
+</div>
+<div>
+<div style="text-align: justify;">
+<h4>
+<b><span style="font-size: large;">الكوكيز وإعدادات الشبكة:</span></b></h4>
+</div>
+</div>
+<div>
+<div style="line-height: 2; text-align: justify;">
+إن شركة <b>Google</b> تستخدم تقنية الكوكيز لتخزين المعلومات عن إهتمامات الزوار، إلى جانب سجل خاص للمستخدم تسجل فيه معلومات محددة عن الصفحات التي تم الوصول إليها أو زيارتها، وبهذه الخطوة فإننا نعرف مدى اهتمامات الزوار وأي المواضيع الأكثر تفضيلا من قبلهم حتى نستطيع بدورنا تطوير محتوانا الخدمي والمعرفي المناسب لهم.</div>
+</div>
+<div>
+<div style="text-align: justify;">
+<br /></div>
+</div>
+<div>
+<div style="line-height: 2; text-align: justify;">
+نضيف إلى ذلك أن بعض الشركات التي تعلن في&nbsp;<a href="https://www.joker0o.de/" target="_blank"><u><b><a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a>
+</b></u></a>&nbsp;قد تتطلع على الكوكيز وإعدادات الشبكة الخاصة بموقعنا وبكم ،&nbsp;ومن هذه الشركات مثلاً شركة <b>Google</b> وبرنامجها الإعلاني <a href="https://accounts.google.com/ServiceLogin?service=adsense&amp;rm=hide&amp;nui=15&amp;alwf=true&amp;ltmpl=adsense&amp;passive=true&amp;continue=https://www.google.com/adsense/gaiaauth2?hl%3Den_US%26subid%3Dww-en_US-et-ads%26sourceid%3Daso%26medium%3Dlink&amp;followup=https://www.google.com/adsense/gaiaauth2?hl%3Den_US%26subid%3Dww-en_US-et-ads%26sourceid%3Daso%26medium%3Dlink&amp;hl=en_US#identifier" target="_blank">Google AdSense</a> وهي شركة الإعلانات الأولى في موقعنا.</div>
+</div>
+<div>
+<div style="text-align: justify;">
+<br /></div>
+</div>
+<div>
+<div style="line-height: 2; text-align: justify;">
+وبالطبع فمثل هذه الشركات المعلنة والتي تعتبر الطرف الثالث في سياسة الخصوصية فهي تتابع مثل هذه البيانات والإحصائيات عبر بروتوكولات الانترنت لأغراض تحسين جودة إعلاناتها وقياس مدى فعاليتها.</div>
+</div>
+<div>
+<div style="text-align: justify;">
+<br /></div>
+</div>
+<div>
+<div style="line-height: 2; text-align: justify;">
+كما أن هذه الشركات بموجب الاتفاقيات المبرمة معنا يحق لها استخدام وسائل تقنية مثل ( الكوكيز ، و إعدادات الشبكة ، و أكواد برمجية خاصة "جافا سكربت" ) لنفس الأغراض المذكورة أعلاه والتي تتلخص في تطوير المحتوى الإعلاني لهذه الشركات وقياس مدى فاعلية هذه الإعلانات ، من دون أي أهداف أخرى قد تضر بشكل أو بآخر على زوار موقعنا.</div>
+</div>
+<div>
+<div style="text-align: justify;">
+<br /></div>
+</div>
+<div>
+<div style="line-height: 2; text-align: justify;">
+وبالطبع فإن موقع&nbsp;<a href="https://www.joker0o.de/" target="_blank"><u><b><a href="${document.querySelector('.URL').value}">${document.querySelector('.Name').value}</a>
+</b></u></a>&nbsp;لا يستطيع الوصول أو السيطرة على هذه الملفات، وحتى بعد سماحك وتفعيلك لأخذها من جهازك (الكوكيز) ، كما أننا غير مسؤولين بأي شكل من الأشكال عن الاستخدام غير الشرعي لها إن حصل ذلك .</div>
+</div>
+<div>
+<div style="text-align: justify;">
+<br /></div>
+</div>
+<div>
+<div style="line-height: 2; text-align: justify;">
+عليك مراجعة سياسة الخصوصية الخاصة بالطرف الثالث في هذه الوثيقة &nbsp;( الشركات المعلنة مثل&nbsp;<a href="https://accounts.google.com/ServiceLogin?service=adsense&amp;rm=hide&amp;nui=15&amp;alwf=true&amp;ltmpl=adsense&amp;passive=true&amp;continue=https://www.google.com/adsense/gaiaauth2?hl%3Den_US%26subid%3Dww-en_US-et-ads%26sourceid%3Daso%26medium%3Dlink&amp;followup=https://www.google.com/adsense/gaiaauth2?hl%3Den_US%26subid%3Dww-en_US-et-ads%26sourceid%3Daso%26medium%3Dlink&amp;hl=en_US#identifier" target="_blank">Google AdSense</a>&nbsp;) أو خوادم الشبكات&nbsp;الإعلانية لمزيد من المعلومات عن ممارساتها وأنشطتها المختلفة .</div>
+</div>
+<div>
+<div style="text-align: justify;">
+<br /></div>
+</div>
+<div style="line-height: 2; text-align: justify;">
+لمراجعة سياسة الخصوصية للبرنامج الإعلاني&nbsp;<a href="https://accounts.google.com/ServiceLogin?service=adsense&amp;rm=hide&amp;nui=15&amp;alwf=true&amp;ltmpl=adsense&amp;passive=true&amp;continue=https://www.google.com/adsense/gaiaauth2?hl%3Den_US%26subid%3Dww-en_US-et-ads%26sourceid%3Daso%26medium%3Dlink&amp;followup=https://www.google.com/adsense/gaiaauth2?hl%3Den_US%26subid%3Dww-en_US-et-ads%26sourceid%3Daso%26medium%3Dlink&amp;hl=en_US#identifier" target="_blank">Google AdSense</a>&nbsp;والتابع لشركة <b>Google </b>يرجى النقر <a href="http://www.google.com/policies/technologies/ads/" target="_blank">هنا</a></div>
+<div>
+<div style="text-align: justify;">
+<br /></div>
+</div>
+<div>
+<div style="line-height: 2; text-align: justify;">
+يُمكن جمع البيانات لتجديد النشاط التسويقي على الشبكة الإعلانية وشبكة البحث عبر &nbsp;سياسة الإعلانات التي تستهدف الاهتمامات والمواقع الخاصة بالزوار ، ويمكن تعطيل هذه الخاصية من <a href="https://tools.google.com/dlpage/gaoptout/?hl=ar" target="_blank">هنا</a>.</div>
+</div>
+<div>
+<div style="text-align: justify;">
+<br /></div>
+</div>
+<div>
+<div style="line-height: 2; text-align: justify;">
+وأخيرا .. نحن ملزمون ضمن بنود هذه الاتفاقية بان نبين لك كيفية تعطيل خاصية الكوكيز ، حيث يمكنك فعل ذلك من خلال خيارات المتصفح الخاص بك، أو من خلال متابعة &nbsp;<a href="http://www.google.com/policies/technologies/ads/" target="_blank">سياسة الخصوصية الخاصة بإعلانات Google وشبكة المحتوى</a> .</div>
+</div>
+<div>
+<div style="text-align: justify;">
+<br /></div>
+</div>
+<div>
+<div style="line-height: 2; text-align: justify;">
+إذا كنت بحاجة إلى مزيد من المعلومات أو لديك أية أسئله عن سياسة الخصوصية ، لا تتردد في الاتصال بنا عن طريق نموذج الإتصال ، بالدخول إلى تبويب <span style="color: red;"><b><a href="https://www.joker0o.de/p/contactform1contact-form-name.html">اتصل بنا</a></b></span>.</div>
+</div>
+<div>
+<div style="text-align: justify;">
+بنود هذه السياسة قابلة للتطوير والتغيير في محتواها في أي وقت نراه ضرورياً .</div>
+</div>
+</div>
+</div>
+<div style="text-align: justify;">
+<br /></div>
+</div>
+</div>
+
    </center><div style="text-align: justify;">`;
 
 document.querySelector('.Code').innerHTML=all;
